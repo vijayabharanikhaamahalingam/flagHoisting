326b242b27256fb00de99fe29f4881819251bca2
viji <vijayabharanikhaamahalingam@gmail.com>
2024-05-10 04:04:00 +0530

    count down

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # flagHoisting
+1. display countdown from 10 to 1 on the display screen [display](index.html)
+2. And then display happy independence day in screen [script](./js/script.js) 
+3. And style using css [styles](./css/styles.css)
---
+++ js/script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("root").textContent=10
+    setTimeout(()=>{
+        document.getElementById("root").textContent=9
+        setTimeout(()=>{
+            document.getElementById("root").textContent=8
+            setTimeout(()=>{
+                document.getElementById("root").textContent=7
+                setTimeout(()=>{
+                    document.getElementById("root").textContent=6
+                    setTimeout(()=>{
+                        document.getElementById("root").textContent=5
+                        setTimeout(()=>{
+                            document.getElementById("root").textContent=4
+                            setTimeout(()=>{
+                                document.getElementById("root").textContent=3
+                                setTimeout(()=>{
+                                    document.getElementById("root").textContent=2
+                                    setTimeout(()=>{
+                                        document.getElementById("root").textContent=1
+                                        setTimeout(()=>{
+                                            document.getElementById("root").textContent="Happy Independence Day"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
